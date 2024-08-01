9ccf2ad26fa2e53dfc3a11b80c2c96e86b9cbcfb
madhu yadav <159256890+madhuyadav5929@users.noreply.github.com>
2024-08-01 14:54:59 +0530

    Update eks.sh

--- eks.sh
+++ eks.sh
@@ -1,20 +1,25 @@
-eksctl create cluster --name=madhu1cluster \
---region=us-east-1 \
---zones=us-east-1a,us-east-1b \
---without-nodegroup
+#!/bin/bash
 
- eksctl create nodegroup \
+# Create the EKS cluster without a node group
+eksctl create cluster \
+  --name madhu1cluster \
+  --region us-east-1 \
+  --zones us-east-1a,us-east-1b \
+  --without-nodegroup
+
+# Create a managed node group for the EKS cluster
+eksctl create nodegroup \
   --cluster madhu1cluster \
   --region us-east-1 \
   --name my-mng \
-  --node-ami-family ami-family \
+  --node-ami-family AmazonLinux2 \
   --node-type t3.medium \
   --nodes 3 \
   --nodes-min 2 \
   --nodes-max 4 \
-  --node-volume-size=20 \
+  --node-volume-size 20 \
   --ssh-access \
-  --ssh-public-key jenkins
+  --ssh-public-key jenkins1 \
   --managed \
   --asg-access \
   --external-dns-access \
